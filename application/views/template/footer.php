7643abd4dc07e0ed8435280bf4dc4b3e744aba8d
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2023-03-12 10:52:50 +0800

    update

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -22,7 +22,7 @@
                         <!-- <p>This website are using :</p> -->
                         <h4 class="modal-title" id="staticBackdropLabel">About This Version</h4>
                         <ul type="square">
-                            <li>Website Version 0.1.0</li>
+                            <li>Website Version 0.1.1</li>
                             <li>Codeigniter Version 3.1.12</li>
                             <li>PHP Version 7.4</li>
                         </ul>
